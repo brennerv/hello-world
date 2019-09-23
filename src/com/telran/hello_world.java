1e29dc1974d85e2ce4ad78fa79dcd6a277949395
Vadim <brenner.v.a@gmail.com>
2019-09-23 21:04:14 +0200

    end hello world

--- src/com/telran/hello_world.java
+++ src/com/telran/hello_world.java
@@ -1,12 +1,17 @@
 package com.telran;
 
+import javax.swing.JOptionPane;
+
 public class hello_world {
 
-    public static final String MY_NAME = "Vadim";
+    public static final String MY_NAME = "Junior";
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!!!");
-        if (args.length > 0) System.out.println("My name is " + MY_NAME + " " + args[0]);
+        if (args.length > 0) {
+            System.out.println("My name is " + MY_NAME + " " + args[0]);
+            JOptionPane.showMessageDialog(null, "My name is " + MY_NAME + " " + args[0],"Dialog:", JOptionPane.PLAIN_MESSAGE);
+        }
     }
 }
