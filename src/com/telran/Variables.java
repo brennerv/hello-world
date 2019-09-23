b178288a1645cc617fa2c550d7a7962d58c9282d
Vadim <brenner.v.a@gmail.com>
2019-09-23 21:16:59 +0200

    added class Variables

--- src/com/telran/Variables.java
+++ src/com/telran/Variables.java
@@ -1,4 +1,17 @@
 package com.telran;
 
+import javax.swing.*;
+import java.sql.SQLOutput;
+
 public class Variables {
+    public static void main(String[] args) {
+        byte a = -128;
+        int b = a;
+        System.out.println(a + " - " + b);
+
+        byte p = 2;
+        byte m = 3;
+
+        System.out.println("Summe: " + (p + m));
+    }
 }
