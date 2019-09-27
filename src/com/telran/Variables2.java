38355a7ca799b7ed74a1d1c9dc8033a1e587a3b8
Brenner <brenner.v.a@googlemail.com>
2019-09-27 21:27:02 +0200

    Added changes

---
+++ src/com/telran/Variables2.java
@@ -0,0 +1,19 @@
+package com.telran;
+
+public class Variables2 {
+	public static void main(String[] args) {
+		double annaApples = 3.2;
+		double peterApples = 4.5;
+
+		double totalAmount = annaApples + peterApples;
+
+		System.out.println(totalAmount);
+
+		float floatNum = 0.4f;
+		float floatNum2 = 0.5f;
+		float floatNumSum = floatNum + floatNum2;
+
+		System.out.println(floatNumSum);
+
+	}
+}
