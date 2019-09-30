d1e9da44069a03e5fe0ed43761291a596c6e4250
Vadim Brenner <brenner.v.a@googlemail.com>
2019-09-30 10:39:03 +0200

    Patch for ReadLine Class

--- src/com/telran/ReadLine.java
+++ src/com/telran/ReadLine.java
@@ -1,12 +1,18 @@
 package com.telran;
 
+import java.util.Scanner;
+
 /**
  * Created by eltern on 26.09.2019.
  */
 public class ReadLine {
     public static void ReadAndPrintString() {
         System.out.println("Gib einen Text ein: ");
-        String text = "Test";// System.console().readLine();
+
+        // Using Scanner for Getting Input from User
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+
         System.out.println("Du hast eingegeben: \"" + text + "\"");
     }
 }
