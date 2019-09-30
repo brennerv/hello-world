e45003d3921c6da2f694af08fde23675093335bc
Vadim Brenner <brenner.v.a@googlemail.com>
2019-09-30 12:15:34 +0200

    Added Home work at class Variables2

--- src/com/telran/DecToBinConverter.java
+++ src/com/telran/DecToBinConverter.java
@@ -5,12 +5,15 @@ package com.telran;
  */
 public class DecToBinConverter {
 	public static void main(String[] args) {
-		int decNum = 1024; // Dec number
-		int copyDecNum = decNum; // Copy
-		String binNum = ""; // Result binary number
-		int pos = 0; // Position
+		int decNum = 371; 			// Dec number
+		int copyDecNum = decNum; 	// Copy
+		String binNum = ""; 		// Result binary number
+		int pos = 0; 				// Position
+		String strDivOnTwo = " | 2";
 
+		System.out.println(decNum + strDivOnTwo);		// print "Number" | 2
 		while (decNum > 0) {
+			System.out.println("-------------------");
 			pos++;
 
 			if (decNum % 2 == 1) {
@@ -18,15 +21,20 @@ public class DecToBinConverter {
 			} else {
 				binNum = "0" + binNum;
 			}
-			if (pos % 8 == 0) binNum = "." + binNum;
 
 			decNum = decNum / 2;
+
+			System.out.println(decNum + strDivOnTwo + "					" + binNum.substring(0, 1));
+
+			if (pos % 8 == 0) binNum = "." + binNum;	// Add point for wole byte
 		}
 
+		// Add "0"'s till whole byte
 		while (pos % 8 != 0) {
 			binNum = "0" + binNum;
 			pos++;
 		}
-		System.out.println("Dec number: " + copyDecNum + " = " + binNum + " in binary.");
+		System.out.println("");
+		System.out.println("Dec number to binary: " + copyDecNum + " = " + binNum + " in binary.");
 	}
 }
--- src/com/telran/Variables2.java
+++ src/com/telran/Variables2.java
@@ -15,5 +15,58 @@ public class Variables2 {
 
 		System.out.println(floatNumSum);
 
+	/*
+		Home work
+		76543210
+		10111000 to dec = 2^7 + 2^5 + 2^4 + 2^3 = 128 + 32 + 16 + 8 = 184
+		371 to bin = b00000001.10011101
+		371 | 2
+		-----------
+			185 | 2     	     					1
+			-------
+				92 | 2								1
+				------
+					46 | 2							0
+					------
+						23 | 2						0
+						------
+							11 | 2					1
+							------
+								5 | 2				1
+								-----
+									2 | 2			1
+									-----
+										1 | 2		0
+										-----
+											0 | 2	1
+	 */
+		System.out.println("");
+		System.out.println("Home work");
+		// With double data type
+		double dblMariaHasApples = 1.0/3.0;
+		double dblPetroHasApples = dblMariaHasApples / 2.0;
+		double dblWasjaHasApples = 4.0 * (dblMariaHasApples + dblPetroHasApples);
+		int intAllAmountOfApples = (int) (dblMariaHasApples + dblPetroHasApples + dblWasjaHasApples);
+
+		System.out.println("Double data type:");
+		System.out.println("Maria has apples = " + dblMariaHasApples);
+		System.out.println("Petro has apples = " + dblPetroHasApples);
+		System.out.println("Wasja has apples = " + dblWasjaHasApples);
+		System.out.println("Amount of whole apples = " + intAllAmountOfApples);
+
+		// With float data type
+		float fltMariaHasApples = 1.0f/3.0f;
+		float fltPetroHasApples = fltMariaHasApples / 2.0f;
+		float fltWasjaHasApples = 4.0f * (fltMariaHasApples + fltPetroHasApples);
+		intAllAmountOfApples = (int) (fltMariaHasApples + fltPetroHasApples + fltWasjaHasApples);
+
+		System.out.println("");
+		System.out.println("Float data type:");
+		System.out.println("Maria has apples = " + dblMariaHasApples);
+		System.out.println("Petro has apples = " + dblPetroHasApples);
+		System.out.println("Wasja has apples = " + dblWasjaHasApples);
+		System.out.println("Amount of whole apples = " + intAllAmountOfApples);
+
+
 	}
 }
